ccff9c7023c8e5dc7ef5fcdc9841a8238176140a
marcusgarcia99 <106205253+marcusgarcia99@users.noreply.github.com>
2024-09-04 19:01:17 +0000

    commit on "main"

--- pages/main_page.py
+++ pages/main_page.py
@@ -7,6 +7,9 @@ with st.sidebar:
     st.sidebar.markdown("# Well Thought Out Thoughts 🎈")
     st.write("Birds are both flying rats and government drones.")
     st.radio("Pick one", ["Agreed", "You need therapy."])
+    st.divider()
+    st.write("A hot dog is a sandwich.")
+    st.radio("Pick one",["Agree", "Totally Agree","Wow, a real genius in the virtual flesh!"])
 
 st.write("Chicken burgah")
 
